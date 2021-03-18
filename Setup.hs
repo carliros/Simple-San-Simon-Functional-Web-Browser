afe0a0ce1c6ef81a68f0f7b2f2f0d828828a9d9c
Carlos Gómez <carliros.g@gmail.com>
2021-03-18 08:20:50 -0400

    Generate ag files and use generated files

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,5 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.UUAGC (uuagcLibUserHook)
+import UU.UUAGC (uuagc)
+
+main = defaultMainWithHooks (uuagcLibUserHook uuagc)
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+import Data.DataTreeHTML (Node(..), NTree(..))
+
+simpleNode :: Node
+simpleNode = NText "text"
+
+simpleTree :: NTree
+simpleTree = NTree simpleNode []
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+  print simpleTree
