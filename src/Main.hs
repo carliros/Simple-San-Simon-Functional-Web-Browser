e2a2a0c9624241b4c03d19c07cd70f00de1c24c9
Carlos Gómez <carliros.g@gmail.com>
2021-03-24 13:05:05 -0400

    Added main boxer windows with Gtk

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import Control.Monad ( void )
+import Control.Monad.IO.Class (liftIO)
+import Graphics.UI.Gtk.Types (Window, WindowClass)
+import Graphics.UI.Gtk 
+  ( AttrOp( (:=))
+  , initGUI, mainGUI, widgetShowAll, deleteEvent, mainQuit
+  , set, on
+  , windowNew, windowTitle, windowDefaultWidth, windowDefaultHeight
+  )
+
 import Data.DataTreeHTML (Node(..), NTree(..))
 
 simpleNode :: Node
@@ -10,5 +20,24 @@ simpleTree = NTree simpleNode []
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
-  print simpleTree
+  window <- initializeMainWindow
+  widgetShowAll window
+  mainGUI
+
+initializeMainWindow :: IO Window
+initializeMainWindow = do
+  void initGUI
+  window <- windowNew
+  setupMainWindows window
+  return window
+
+setupMainWindows :: WindowClass win => win -> IO ()
+setupMainWindows window = do
+  set window [ windowTitle          := "Boxer"
+             , windowDefaultWidth   := 800
+             , windowDefaultHeight  := 600
+             ]
+  window `on` deleteEvent $ do
+    liftIO mainQuit 
+    return False
+  return ()
