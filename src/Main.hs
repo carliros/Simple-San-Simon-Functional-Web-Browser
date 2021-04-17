7df0d74f85ba3ec2650bf19244f64c4c648c0e31
Carlos Gómez <carliros.g@gmail.com>
2021-04-16 22:36:44 -0400

    Added main layout for boxer

--- src/Main.hs
+++ src/Main.hs
@@ -9,9 +9,12 @@ import Graphics.UI.Gtk
   , set, on
   , windowNew, windowTitle, windowDefaultWidth, windowDefaultHeight
   , entryNew, entryText
+  , buttonNewWithLabel
   , containerAdd
   )
+import Graphics.UI.Gtk.Abstract.Box ( Packing(..), boxPackStart )
 import Graphics.UI.Gtk.Layout.VBox ( vBoxNew )
+import Graphics.UI.Gtk.Layout.HBox ( hBoxNew )
 import Graphics.UI.Gtk.Layout.Layout ( layoutNew, layoutWidth, layoutHeight )
 
 import Data.DataTreeHTML (Node(..), NTree(..))
@@ -27,15 +30,18 @@ main :: IO ()
 main = do
   defaultcss4html <- parseFileUserAgent "./config/html4.css"
   window <- initializeMainWindow
-  vBox <- vBoxNew True 5
+  vBox <- vBoxNew False 5
   
-  -- display url
-  display <- entryNew
-  containerAdd vBox display
+  -- display url with button
+  hBox <- hBoxNew False 5
+  urlBox <- entryNew
+  boxPackStart hBox urlBox PackGrow 5
+  getButton <- buttonNewWithLabel "Get"
+  boxPackStart hBox getButton PackNatural 5
+  boxPackStart vBox hBox PackNatural 5
   -- main box content
   mainContent <- layoutNew Nothing Nothing
-  set mainContent [ layoutHeight := 300 ]
-  containerAdd vBox mainContent
+  boxPackStart vBox mainContent PackGrow 5
 
   containerAdd window vBox
   widgetShowAll window
